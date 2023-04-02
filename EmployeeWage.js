b8133dda0e21c7347c053313aa7cf35a805de06e
rinkuberde <rinkuberde@gmail.com>
2023-04-02 22:52:04 +0530

    [Rinku]:Calculate Wages for a Month

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,10 +1,11 @@
-// UC-03 Function for daily working Hours
-
+//UC 4 Calculating Wages for a Month
 const IS_PART_TIME=1;
 const IS_FULL_TIME=2;
 const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOUR=20;
+const NUM_OF_WORKING_DAYS=20;
+
 function getWorkingHours(empCheck)
 {
   switch(empCheck)
@@ -17,10 +18,12 @@ function getWorkingHours(empCheck)
           return 0;
   }
 }
-{
-      let empHrs=0;
-      let empCheck=Math.floor(Math.random()*10)%3;
-      empHrs=getWorkingHours(empCheck);
-      let empWage=empHrs*WAGE_PER_HOUR;
-      console.log(" Hour: " +empHrs+ " Emp Wage: "+empWage);
-}
+ let totalEmpHrs=0;
+ for(let day=0;day<NUM_OF_WORKING_DAYS;day++)
+ {
+   let empCheck=Math.floor(Math.random()*10)%3;
+   totalEmpHrs+=getWorkingHours(empCheck);
+ }
+ let empWage=totalEmpHrs*WAGE_PER_HOUR;
+ console.log("Hour: "+totalEmpHrs+" Emp Wage: "+empWage);
+ 
