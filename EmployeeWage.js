fac6d18b94d3c2a34eefef242f3d6dd9ef4ae142
rinkuberde <rinkuberde@gmail.com>
2023-04-02 22:42:44 +0530

    [Rinku]:Calculate Employee Wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,13 +1,25 @@
-console.log("Welcome to Employee Wage Using JS!!!")
-//UC-01 Check Employee is present or Absent 
-const IS_ABSENT=0;
-let empCheck = Math.floor(Math.random()*10)%2;
-if(empCheck==IS_ABSENT)
+// UC-02 Calculating Employee Wage
+const IS_PART_TIME=1;
+const IS_FULL_TIME=2;
+const PART_TIME_HOURS=4;
+const FULL_TIME_HOURS=8;
+const WAGE_PER_HOUR=20;
+
+let empHrs=0;
+let empCheck=Math.floor(Math.random()* 10 % 3);
+switch(empCheck)
 {
-    console.log("Employee is absent");
+ case IS_PART_TIME:
+      //employee is present and part-time
+      empHrs=PART_TIME_HOURS;
+      break;
+case IS_FULL_TIME:
+      //employee is present and full time
+      empHrs=FULL_TIME_HOURS;
+      break;
+default:
+      empHrs=0;
 }
-else
-{
-    console.log("Employee is present"); 
-}
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Emp wage: "+empWage);
 
